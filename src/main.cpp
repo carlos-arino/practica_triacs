585fef71ac8726f54452113c2b10b9847c949b95
carlos-arino <arino@uji.es>
2025-12-16 23:03:56 +0100

    Formatear el código en main.cpp para mejorar la legibilidad

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <LiquidCrystal.h>
+#include <Arduino.h>
 
-LiquidCrystal lcd(12,13,17,16,27,14);
+LiquidCrystal lcd(12, 13, 17, 16, 27, 14);
 
-void setup() {  
+void setup()
+{
   pinMode(26, INPUT);
   pinMode(25, OUTPUT);
-  lcd.begin(16, 2);  
+  lcd.begin(16, 2);
 }
 
-void loop() {  
-  digitalWrite(25,1);
+void loop()
+{
+  digitalWrite(25, 1);
   delay(1000);
-  digitalWrite(25,0);
+  digitalWrite(25, 0);
   delay(1000);
 }
